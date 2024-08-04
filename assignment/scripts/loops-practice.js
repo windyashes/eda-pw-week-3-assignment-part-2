b0a8930e2be554b7188cefcb5aa6dda1aeb4d783
windyashes <sethkemmet@gmail.com>
2024-08-04 12:09:31 -0500

    Actually did the for of loop how I was supposed to do it

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -46,8 +46,8 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-for(let i = 0; i < stars.length; i++){
-  console.log(stars[i]);
+for(let i of stars){
+  console.log(i);
 }
 
 // 3. While loops  
